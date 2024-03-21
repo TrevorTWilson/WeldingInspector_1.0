66c0cef2b74bd504814f71a472259756eb38aeec
Trevor Wilson <trevortwilson@hotmail.com>
2024-03-21 06:28:09 -0700

    Initial Commit

---
+++ WeldingInspector/WeldingInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeldingInspectorApp.swift
+//  WeldingInspector
+//
+//  Created by trevor wilson on 2024-03-21.
+//
+
+import SwiftUI
+
+@main
+struct WeldingInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
